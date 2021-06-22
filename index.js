928516dddd20e1d3dd0bcc6546f279b5fa8a073c
nickjjang <nickjang234@gmail.com>
2021-06-22 09:56:41 -0400

    env

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-function sayHello() {
-  console.log('Hello');
+require("dotenv").config();
+const Web3 = require("web3");
+const abi = require("./abi/strategybase.json");
+
+async function contractcall() {
+  let web3 = new Web3(process.env.PROVIDER);
+  const contract = new web3.eth.Contract(abi, process.env.CONTRACT_ADDRESS);
+  const gasPrice = await web3.eth.getGasPrice();
+  web3.eth.accounts.wallet.add({
+    privateKey: process.env.OWNER_PRIVKEY,
+    address: process.env.OWNER_ADDRESS,
+  });
+
+  await contract.methods
+    .harvest()
+    .send({
+      from: process.env.OWNER_ADDRESS,
+      gasPrice,
+      gas: 5000000
+    });
 }
-sayHello();
+
+contractcall();
