9ae366e292f15bf55f54a026e82b5e29b185573c
nickjjang <nickjang234@gmail.com>
2021-06-22 12:02:26 -0400

    multi harvest added

--- index.js
+++ index.js
@@ -3,21 +3,36 @@ const Web3 = require("web3");
 const abi = require("./abi/strategybase.json");
 
 async function contractcall() {
-  let web3 = new Web3(process.env.PROVIDER);
-  const contract = new web3.eth.Contract(abi, process.env.CONTRACT_ADDRESS);
-  const gasPrice = await web3.eth.getGasPrice();
-  web3.eth.accounts.wallet.add({
-    privateKey: process.env.OWNER_PRIVKEY,
-    address: process.env.OWNER_ADDRESS,
-  });
+  const provider = process.env.PROVIDER;
+  const contractAddrsStr = process.env.CONTRACT_ADDRESSES;
+  const contractAddrs = contractAddrsStr.split(",");
+  const ownerPrivKey = process.env.OWNER_PRIVKEY;
+  const ownerAddr = process.env.OWNER_ADDRESS;
 
-  await contract.methods
-    .harvest()
-    .send({
-      from: process.env.OWNER_ADDRESS,
+  const web3 = new Web3(provider);
+  web3.eth.accounts.wallet.add({
+    privateKey: ownerPrivKey,
+    address: ownerAddr,
+  });
+  
+  const gasPrice = await web3.eth.getGasPrice();
+  console.log(`Gas price is ${gasPrice}`);
+
+  console.log('Multi harvest started.');
+  const harvest = async (contractAddr) => {
+    const contract = new web3.eth.Contract(abi, contractAddr);
+    await contract.methods.harvest().send({
+      from: ownerAddr,
       gasPrice,
-      gas: 5000000
+      gas: 5000000,
     });
+  }
+  const multiHarvest = [];
+  for (let i = 0, ni = contractAddrs.length; i < ni; i++) {
+    multiHarvest.push(harvest(contractAddrs[i].trim()))
+  }
+  await Promise.all(multiHarvest);
+  console.log('Multi harvest completed.');
 }
 
 contractcall();
